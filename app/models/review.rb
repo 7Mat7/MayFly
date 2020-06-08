5bc2262f68ab57858e5cc11ef3c075336f16865b
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-08 18:19:41 +0200

    modify review model to validation correct

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :guest, foreign_key: :user_id
   belongs_to :place
 
-  validates :rating, presence: true
-  validates numericality: { only_integer: true }
+  validates :rating, presence: true, numericality: { only_integer: true }
 end
