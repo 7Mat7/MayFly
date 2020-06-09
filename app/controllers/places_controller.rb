15a8d15fb19dcf6122456ca76d1c6b2b30b89b82
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-09 12:04:51 +0200

    index and edit/destroy/create

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,25 +1,52 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [ :show, :edit, :update, :destroy]
+
   def index
+    @places = Place.all
   end
 
   def show
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    if @place.save!
+      redirect_to place_path(@place), notice: 'Place was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @place.update(place_params)
+      redirect_to @place, notice: 'Place was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @place.destroy
+    redirect_to places_url, notice: 'Place was successfully destroyed.'
   end
 
   def list
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:description, :price, :location, :size, :renter_id)
   end
 end
