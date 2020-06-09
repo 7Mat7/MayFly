ef17d7be016787e7ab079e9dc644b24ebed1b5f7
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-09 15:49:01 +0200

    pundit correction create authorization

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,12 +11,13 @@ class PlacesController < ApplicationController
 
   def new
     @place = Place.new
-     authorize(@place)
+    authorize(@place)
   end
 
   def create
     @place = Place.new(place_params)
     @place.renter = current_user
+    authorize(@place)
     if @place.save!
       redirect_to place_path(@place), notice: 'Place was successfully created.'
     else
