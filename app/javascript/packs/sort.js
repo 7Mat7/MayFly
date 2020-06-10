2cd8dfa3991e9d21ac65947f0ee021cfffb0d24c
Mateo Blanc <mateoblanc93@gmail.com>
2020-06-10 12:43:05 +0200

    order by size

---
+++ app/javascript/packs/sort.js
@@ -0,0 +1,16 @@
+const size = document.getElementById('size')
+const price = document.getElementById('price')
+const available = document.getElementById('available')
+
+const list = document.getElementById('list')
+
+size.addEventListener('click', (e) => {
+  const listedItemSize = document.querySelectorAll('.listed-place');
+  const arrayFromNodeList = Array.from(listedItemSize);
+  arrayFromNodeList.sort((a, b) => {
+    return parseInt(a.dataset.size) - parseInt(b.dataset.size);
+  })
+  list.innerHTML = ''
+  arrayFromNodeList.forEach(e => list.appendChild(e))
+})
+
